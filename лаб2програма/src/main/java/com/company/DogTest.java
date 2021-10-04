d2a5ee5b027defd418455b15bec148bde2b0e439
maxyamba <50770176+maxyamba@users.noreply.github.com>
2021-10-04 14:24:15 +0300

    Add files via upload

---
+++ лаб2програма/src/main/java/com/company/DogTest.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DogTest {
+
+    @org.junit.jupiter.api.Test
+    void testGetDogNameMethod() {
+        Dog dog = new Dog("Dima",3);
+        assertEquals("Dima", dog.getName());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetDogAgeMethod() {
+        Dog dog = new Dog("Fasa",3);
+        assertEquals(3, dog.getAge());
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAge() {
+    }
+}
